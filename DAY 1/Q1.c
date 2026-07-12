c910dd7710e25550c6e0d4599da084d065523a13
PriyaYadav-2025 <priya.yadav2025@glbajajgroup.org>
2026-07-12 18:02:53 +0530

    Update Q1.c

--- DAY 1/Q1.c
+++ DAY 1/Q1.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main()
+{
+    int i ,n,sum=0;
+    printf("Enter the number");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+      sum+=i;
+    printf("sum of first %d natural numbers is %d",n,sum);
+    return 0;
+}
+  
