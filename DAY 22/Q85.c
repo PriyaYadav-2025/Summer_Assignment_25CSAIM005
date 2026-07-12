265ed8b9b2852265ee9cbc0124df38beef775238
PriyaYadav-2025 <priya.yadav2025@glbajajgroup.org>
2026-07-12 22:55:13 +0530

    Update Q85.c

--- DAY 22/Q85.c
+++ DAY 22/Q85.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100];
+    int i, j, c = 1;
+    printf("Enter a string: ");
+    gets(str);
+    j = strlen(str)-1;
+    for(i=0; i<j; i++,j--)
+    {
+        if(str[i] != str[j])
+        {
+            c = 0;
+            break;
+        }
+    }
+    if(c)
+        printf("Palindrome string");
+    else
+        printf("Not a palindrome string");
+    return 0;
+}
