ed7406848fa3221d5ced70cb979dc22b211e168b
PriyaYadav-2025 <priya.yadav2025@glbajajgroup.org>
2026-07-12 19:26:17 +0530

    Update Q53.c

--- DAY 14/Q53.c
+++ DAY 14/Q53.c
@@ -1,1 +1,26 @@
-
+#include <stdio.h>
+int main() 
+{
+    int arr[100], n, i, key, c = 0;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+    for (i = 0; i < n; i++) 
+    {
+        if (arr[i] == key) 
+        {
+            c = 1;
+            printf("Element found at position %d", i + 1);
+            break;
+        }
+    }
+    if (c == 0)
+        printf("Element not found");
+    return 0;
+}
