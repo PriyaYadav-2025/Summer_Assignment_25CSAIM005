ca1090fc3c15817b39023579d29890cd69452520
PriyaYadav-2025 <priya.yadav2025@glbajajgroup.org>
2026-07-12 19:18:43 +0530

    Update Q45.c

--- DAY 12/Q45.c
+++ DAY 12/Q45.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+int palindrome(int n)
+{
+    int pal = 0, temp = n,rem;
+    while(n > 0)
+    {   
+        rem=n%10;
+        pal = pal* 10 + rem;
+        n = n / 10;
+    }
+    return temp == pal;
+}
+int main()
+{
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    if(palindrome(n))
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
 
