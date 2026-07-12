30c0fde3c98eef413198c59971066bd710dfa14d
PriyaYadav-2025 <priya.yadav2025@glbajajgroup.org>
2026-07-12 18:31:23 +0530

    Update Q17.c

--- DAY 5/Q17.c
+++ DAY 5/Q17.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+int main()
+{
+    int i, n, sum = 0;
+    printf("Enter any number  ");
+    scanf("%d", &n);
+    for(i = 1; i <= n ; i++)
+    {
+      if(n%i == 0)
+      {
+        sum += i;
+      }
+    }
+    if(sum == n && n > 0)
+    {
+     printf("%d is PERFECT NUMBER", n);
+    }
+    else
+    {
+     printf("%d is NOT PERFECT NUMBER", n);
+    }
+    return 0;
+}
