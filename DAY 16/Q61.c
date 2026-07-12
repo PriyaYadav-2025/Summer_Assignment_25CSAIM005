4289f07b1ab23908dfdfe97ec0316d10f62a2984
PriyaYadav-2025 <priya.yadav2025@glbajajgroup.org>
2026-07-12 19:35:01 +0530

    Update Q61.c

--- DAY 16/Q61.c
+++ DAY 16/Q61.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+int main()
+{
+    int arr[100], n, i, sum = 0, total;
+    printf("Enter size of array");
+    scanf("%d",&n);
+    printf("Enter %d elements:\n", n-1);
+    for(i=0;i<n-1;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum += arr[i];
+    }
+    total = n * (n + 1) / 2;
+    printf("Missing number is: %d", total - sum);
+    return 0;
+}
