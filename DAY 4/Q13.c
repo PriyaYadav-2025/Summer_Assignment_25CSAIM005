d94c86fea083a0dbb9c5fe33699bf458c709cb44
PriyaYadav-2025 <priya.yadav2025@glbajajgroup.org>
2026-07-12 18:26:53 +0530

    Update Q13.c

--- DAY 4/Q13.c
+++ DAY 4/Q13.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+int main()
+ {
+    int i, n,t3;
+    int t1 = 0, t2 = 1;
+    t3 = t1 + t2;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: %d, %d, ", t1, t2);
+    for (i = 3; i <= n; ++i)
+     {
+      printf("%d, ", t3);
+      t1 = t2;
+      t2 = t3;
+      t3 = t1 + t2;
+     }
+    return 0;
+}
 
