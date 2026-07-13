bfef78b8f0cffce89161271e04a905444758f9aa
PriyaYadav-2025 <priya.yadav2025@glbajajgroup.org>
2026-07-13 10:25:20 +0530

    Update Q101.c

--- DAY 26/Q101.c
+++ DAY 26/Q101.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main() 
+{
+    int secret = 25, guess;
+    printf("Guess the number (1-50): ");
+    scanf("%d", &guess);
+    if(guess == secret)
+        printf("Correct! You guessed it.\n");
+    else if(guess < secret)
+        printf("Too Low.\n");
+    else
+        printf("Too High.\n");
+    return 0;
+}
