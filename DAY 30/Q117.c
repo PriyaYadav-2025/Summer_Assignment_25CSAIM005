5f0a2eaffa2bd16e22dda8e4183115f901b522f8
PriyaYadav-2025 <priya.yadav2025@glbajajgroup.org>
2026-07-13 11:37:17 +0530

    Update Q117.c

--- DAY 30/Q117.c
+++ DAY 30/Q117.c
@@ -1,1 +1,51 @@
+#include <stdio.h>
+
+struct Student
+{
+    int roll;
+    char name[30];
+    float marks;
+};
+int main()
+{
+    struct Student s[50];
+    int n, i, search, found = 0;
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+    for(i = 0; i < n; i++)
+    {
+        printf("\nEnter Roll Number: ");
+        scanf("%d", &s[i].roll);
+        printf("Enter Name: ");
+        scanf("%s", s[i].name);
+        printf("Enter Marks: ");
+        scanf("%f", &s[i].marks);
+    }
+    printf("\n--- Student Records ---\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("\nRoll No: %d", s[i].roll);
+        printf("\nName: %s", s[i].name);
+        printf("\nMarks: %.2f\n", s[i].marks);
+    }
+
+    printf("\nEnter roll number to search: ");
+    scanf("%d", &search);
+    for(i = 0; i < n; i++)
+    {
+        if(s[i].roll == search)
+        {
+            printf("\nStudent Found");
+            printf("\nName: %s", s[i].name);
+            printf("\nMarks: %.2f", s[i].marks);
+            found = 1;
+            break;
+        }
+    }
+    if(found == 0)
+    {
+        printf("\nStudent not found");
+    }
+    return 0;
+}
 
